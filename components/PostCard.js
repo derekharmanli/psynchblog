25c1aa3f4cd09993a446fec969efa3a7a17cde85
Derek Harmanli <derek.harmanli@gmail.com>
2023-12-01 13:03:58 -0500

    made the featured image on postcard a link as well

--- components/PostCard.js
+++ components/PostCard.js
@@ -7,7 +7,9 @@ const PostCard = ({post}) => {
     <>
     <div className= 'bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
         <div className = "relative overflow-hidden shadow-md pb-80 mb-6">
+          <Link href ={`/post/${post.slug}`}>
             <img src ={post.featuredImage.url} alt={post.title} className = "object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"/>
+          </Link>
         </div>
         <h1 className = "transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold"> 
           <Link href ={`/post/${post.slug}`}>
