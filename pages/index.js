74b2595948b7aa9d771c0ee52f6c14518369ab65
Derek Harmanli <derek.harmanli@gmail.com>
2023-12-01 14:00:40 -0500

    Added shadow

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,36 @@
-
-import Head from 'next/head'
-import { PostCard, PostWidget, Categories } from '../components'
+import Head from 'next/head';
+import { PostCard, PostWidget, Categories } from '../components';
 import { FeaturedPosts } from '../sections/index';
-import {GetPosts} from '../services'
+import { GetPosts } from '../services';
 
-export default function Home( {posts}) {
+export default function Home({ posts }) {
   return (
-   <div className= "container mx-auto px-10 mb-8">
+    <div className="container mx-auto px-10 mb-8">
       <FeaturedPosts />
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className= "lg:col-span-8 col-span-1">
-            {posts.map ((post)=> (<PostCard post={post.node} key={post.title}/> ))}
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <div className="shadow-md p-4 mb-4" key={post.title}>
+              <PostCard post={post.node} />
+            </div>
+          ))}
         </div>
-      
-        <div className ="lg:col-span-4 col-span-1">
-              <div className = "lg:sticky relative top-8">
-                <PostWidget />
-                <Categories />
-              </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="shadow-md p-4 mb-4 lg:sticky relative top-8">
+            <PostWidget />
+          </div>
+          <div className="shadow-md p-4 mb-4 lg:sticky relative top-8">
+            <Categories />
+          </div>
         </div>
       </div>
-     
-
-   </div>
-  )
+    </div>
+  );
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   const posts = (await GetPosts()) || [];
   return {
-    props: {posts}
-  }
+    props: { posts },
+  };
 }
-
