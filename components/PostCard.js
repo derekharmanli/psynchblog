f5bfd998e2ac74cba589752a4b120398ea0b3a7c
Derek Harmanli <derek.harmanli@gmail.com>
2023-12-02 08:56:32 -0500

    fixed a margin issue on small screens on postcard

--- components/PostCard.js
+++ components/PostCard.js
@@ -17,17 +17,17 @@ const PostCard = ({post}) => {
           </Link>
         </h1>
         <div className="block flex-col text-center justify-center mb-8 w-full">
-          <div className="flex lg:justify-center mb-4 w-full lg:w-auto mr-8">
+          <div className="flex lg:justify-center mb-4 w-full lg:w-auto">
             <div className="flex lg:items-center">
               <img alt={post.author.name} height="30px" width="30px" className="rounded-full" src={post.author.photo.url} />
-              <p className="inline text-gray-700 ml-2 text-lg">{post.author.name}</p>
+              <p className="inline text-gray-700 lg:ml-2 ml-10 text-lg">{post.author.name}</p>
             </div>
           </div>
           <div className="flex lg:items-center lg:justify-center mb-4">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
-            <span className= "ml-2">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+            <span className= "lg:ml-2 ml-10">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
           </div>
           <div className="flex items-center lg:justify-center">
             <img alt={post.categories[0].name} height="30px" width="30px" className="rounded-full" src={post.categories[0].picture.url} />
