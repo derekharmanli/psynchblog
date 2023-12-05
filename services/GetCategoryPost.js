e73566458c9ef49e500fa2c793c831b49d4a841f
Derek Harmanli <derek.harmanli@gmail.com>
2023-12-05 12:14:53 -0500

    fixed category view with pagination

--- services/GetCategoryPost.js
+++ services/GetCategoryPost.js
@@ -1,45 +1,72 @@
-import {request, gql} from 'graphql-request';
+import { request, gql } from 'graphql-request';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 const GetCategoryPost = async (slug) => {
-  const query = gql`
-    query GetCategoryPost($slug: String!) {
-      postsConnection(where: {categories_some: {slug: $slug}}) {
-        edges {
-          cursor
-          node {
-            author {
-              bio
-              name
-              id
-              photo {
+  const pageSize = 10; // Adjust the page size as needed
+
+  const fetchCategoryPosts = async (after) => {
+    const query = gql`
+      query GetCategoryPosts($slug: String!, $pageSize: Int!, $after: String) {
+        postsConnection(where: { categories_some: { slug: $slug } }, first: $pageSize, after: $after) {
+          edges {
+            cursor
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
                 url
               }
-            }
-            createdAt
-            slug
-            title
-            excerpt
-            featuredImage {
-              url
-            }
-            categories {
-              name
-              slug
-              picture {
-                url
+              categories {
+                name
+                slug
+                picture {
+                  url
+                }
               }
             }
           }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
         }
       }
-    }
-  `;
+    `;
 
-  const result = await request(graphqlAPI, query, { slug });
+    const variables = {
+      slug,
+      pageSize,
+      after,
+    };
 
-  return result.postsConnection.edges;
+    const result = await request(graphqlAPI, query, variables);
+    return result.postsConnection;
+  };
+
+  let allCategoryPosts = [];
+  let hasMorePages = true;
+  let afterCursor = null;
+
+  while (hasMorePages) {
+    const categoryPosts = await fetchCategoryPosts(afterCursor);
+    allCategoryPosts = [...allCategoryPosts, ...categoryPosts.edges]
+
+    hasMorePages = categoryPosts.pageInfo.hasNextPage;
+    afterCursor = categoryPosts.pageInfo.endCursor;
+  }
+
+  return allCategoryPosts.reverse();
 };
 
-export default GetCategoryPost
+export default GetCategoryPost;
